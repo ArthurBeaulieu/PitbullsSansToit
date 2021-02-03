49836aa514594301fa532bce4abb677dc8e503aa
ArthurBeaulieu <contact@abeaulieu.fr>
2021-02-03 20:30:47 +0100

    webpack 5

---
+++ src/js/APST.js
@@ -0,0 +1,20 @@
+import '../scss/apst.scss';
+
+
+class APST {
+
+
+  constructor() {
+
+  }
+
+
+  init() {
+    console.log('Hello world\n');
+  }
+
+
+}
+
+
+export default APST;
---
+++ src/tools/webpack.common.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const webpack = require('webpack');
+const loaders = require('./loaders');
+const plugins = require('./plugins');
+
+module.exports = {
+  entry: ['./src/js/APST.js'],
+  module: {
+    rules: [
+      loaders.JSLoader,
+      loaders.CSSLoader
+    ]
+  },
+  output: {
+    filename: 'apst.bundle.js',
+    path: path.resolve(__dirname, '../../assets/dist'),
+    library: 'APST', // We set a library name to bundle the export default of the class
+    libraryTarget: 'window', // Make it globally available
+    libraryExport: 'default' // Make APST.default become APST
+  },
+  plugins: [
+    new webpack.ProgressPlugin(),
+    plugins.CleanWebpackPlugin,
+    plugins.ESLintPlugin,
+    plugins.StyleLintPlugin,
+    plugins.MiniCssExtractPlugin
+  ]
+};
