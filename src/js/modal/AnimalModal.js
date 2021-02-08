1764a13ff688894bcea41b0a453e88f1515c080c
ArthurBeaulieu <contact@abeaulieu.fr>
2021-02-08 20:41:49 +0100

    improved some layout

--- src/js/modal/AnimalModal.js
+++ src/js/modal/AnimalModal.js
@@ -27,8 +27,7 @@ class AnimalModal extends Modal {
 		        <span><i class="fa fa-cat ${this._info.affinity.cats}"></i>${this._info.affinity.cats.toUpperCase()} chats</span><br>
 		        <span><i class="fa fa-baby ${this._info.affinity.baby}"></i>${this._info.affinity.baby.toUpperCase()} enfants</span>
 	        </p>
-	        <p class="desc">Vous voulez changer la vie de cet animal? Consultez la page "À l'adoption" pour plus d'information, ou n'hésitez pas à nous contacter pour vous positionnez pour une adoption!</p>
-					<p><i><a href="tel:33619270852"><b>+33 6 19 27 08 52</b></a> ou par <a href="mailto:contact@pitbulls-sans-toit.com"><b>mail</b></a></i></p>
+	        <p class="desc">Vous voulez changer la vie de cet animal? Consultez la page <a href="adoption.html">Adoption</a> pour plus d'information sur les procédures d'adoption, ou n'hésitez pas à nous contacter pour vous positionner pour ce dernier!</p>					
 	      </div>
 	    </div>`;
   }
